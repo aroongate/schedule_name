5e4626f60ac2091bdeccf708dbad5ded7ff9a211
PsyOnFire <e.borovitin@yandex.ru>
2022-11-03 21:22:34 +0300

    почти рабочий скрипт

--- pervaya straniza/js/dobavlenie_prepoda.js
+++ pervaya straniza/js/dobavlenie_prepoda.js
@@ -1,6 +1,12 @@
-document.querySelector("#dobavit").onclick = function(){
-    var clonedNode = document.getElementById("test").cloneNode(true);
-    document.getElementsByClassName('test__prepoda')[0].style= "top:297px";
-    document.getElementsByClassName('test__prepoda__nadpis')[0].style= "top:+10px";
-    console.log(clonedNode)
+
+
+ document.querySelector('.out').onclick = function(){
+   // var clonedNode = document.getElementById("test").cloneNode(true);
+   // document.getElementsByClassName('test__prepoda')[0].style= "top:297px";
+   // document.getElementsByClassName('test__prepoda__nadpis')[0].style= "top:+10px";
+   // console.log(clonedNode)
+    let a = document.createElement('div');
+    a.className='test__prepoda__nadpis';
+    let b = document.createElement('button');
+    b.className='krestik';
 }
