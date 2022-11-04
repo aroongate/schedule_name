b5ebeb2cb4b5af5fc5ab92b90057a938c7d2f8bd
PsyOnFire <e.borovitin@yandex.ru>
2022-11-04 20:14:45 +0300

    осталось совсем немного(нет)

--- pervaya straniza/js/dobavlenie_prepoda.js
+++ pervaya straniza/js/dobavlenie_prepoda.js
@@ -1,12 +1,35 @@
 
 
- document.querySelector('.out').onclick = function(){
+ document.querySelector('#out').onclick = function(){
    // var clonedNode = document.getElementById("test").cloneNode(true);
    // document.getElementsByClassName('test__prepoda')[0].style= "top:297px";
    // document.getElementsByClassName('test__prepoda__nadpis')[0].style= "top:+10px";
    // console.log(clonedNode)
     let a = document.createElement('div');
-    a.className='test__prepoda__nadpis';
-    let b = document.createElement('button');
-    b.className='krestik';
+    a.className='test__prepoda';
+    a.id="a";
+    document.body.append(a);
+    let c =document.createElement('div')
+    c.className='test__prepoda__nadpis__2'
+    c.id="c"
+    document.body.append(c);
+    let b = document.createElement('button', function(){
+      
+      let elem = document.getElementById("qe");
+      elem.parentNode.remove(elem)
+      //   let elem2 = document.getElementById("krestik");
+      // elem.parentNode.removeChild(elem2)
+      //   let elem3 = document.getElementById("c");
+      // elem.parentNode.removeChild(elem3)
+        
+  } );
+  b.className='krestik__2';
+  b.id='krestik'
+    
+    document.body.append(b);
+    
+    console.log(a)
+    console.log(b)
+    console.log(c)
+    
 }
--- pervaya straniza/js/udalenie.js
+++ pervaya straniza/js/udalenie.js
@@ -1,4 +1,8 @@
 document.querySelector("#krestik").onclick = function(){
-    let elem = document.getElementById("prepod1");
+    let elem = document.getElementById("test__prepoda");
 	elem.parentNode.removeChild(elem)
+    let elem2 = document.getElementById("test__prepoda__nadpis__2");
+	elem.parentNode.removeChild(elem2)
+    let elem3 = document.getElementById("krestik");
+	elem.parentNode.removeChild(elem3)
 }
