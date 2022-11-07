cda01b07d9321bdbcb05d164b80d2eb1a3152efc
PsyOnFire <e.borovitin@yandex.ru>
2022-11-07 16:47:51 +0300

    test

--- pervaya straniza/js/dobavlenie_prepoda.js
+++ pervaya straniza/js/dobavlenie_prepoda.js
@@ -1,35 +1,14 @@
-
-
- document.querySelector('#out').onclick = function(){
-   // var clonedNode = document.getElementById("test").cloneNode(true);
-   // document.getElementsByClassName('test__prepoda')[0].style= "top:297px";
-   // document.getElementsByClassName('test__prepoda__nadpis')[0].style= "top:+10px";
-   // console.log(clonedNode)
-    let a = document.createElement('div');
-    a.className='test__prepoda';
-    a.id="a";
-    document.body.append(a);
-    let c =document.createElement('div')
-    c.className='test__prepoda__nadpis__2'
-    c.id="c"
-    document.body.append(c);
-    let b = document.createElement('button', function(){
-      
-      let elem = document.getElementById("qe");
-      elem.parentNode.remove(elem)
-      //   let elem2 = document.getElementById("krestik");
-      // elem.parentNode.removeChild(elem2)
-      //   let elem3 = document.getElementById("c");
-      // elem.parentNode.removeChild(elem3)
-        
-  } );
-  b.className='krestik__2';
-  b.id='krestik'
-    
-    document.body.append(b);
-    
-    console.log(a)
-    console.log(b)
-    console.log(c)
-    
-}
+const request = new Request("http://127.0.0.1:8000/api/teachers", {
+    method: "GET",
+  });
+  request.headers.set(
+    "Authorization",
+    "Bearer " + "2|L4M08TofVC9uNxADIiVF8VSuSlmCF8PdBD8wZyIG"
+  );
+  const url = "http://127.0.0.1:8000/api/teachers";
+fetch(url)
+  .then(
+    response => response.text() // .json(), .blob(), etc.
+  ).then(
+    text => console.log(text) // Handle here
+  );
