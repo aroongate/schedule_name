e0a45cb88294e01f50278ff7225c06d2e380767b
PsyOnFire <e.borovitin@yandex.ru>
2022-11-07 17:56:00 +0300

    теперь попробуй это

--- pervaya straniza/js/dobavlenie_prepoda.js
+++ pervaya straniza/js/dobavlenie_prepoda.js
@@ -8,9 +8,10 @@ let mem = fetch("http://127.0.0.1:8000/api/admin/get_teachers", {
   .then((response) => response.json())
   .then((data) => {
     let authors = data.results;
+    return authors.map(function(author) {
     let span ;
-    span.innerHTMl = `$(authors.first_name) $(authors.last_name) $(authors.patronymic_name)`;
+    span.innerHTMl = `$(author.first_name) $(author.last_name) $(author.patronymic_name)`;
     console.log(span);
   });
 
-
+})
